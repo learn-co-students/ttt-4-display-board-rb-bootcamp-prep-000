874881b85c5e730da5daf02581ff2051f1a0ec02
aviflombaum <avi@flombaum.com>
2015-10-08 00:59:21 -0400

    Fix pending tests by forcing them to pass

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -55,7 +55,7 @@ describe "#display_board in 'lib/display_board.rb" do
       expect(output).to include("-----------")
       expect(output).to include("   | X |   ")
       expect(output).to include("-----------")
-      expect(output).to include("   |   |   ")      
+      expect(output).to include("   |   |   ")
     end
 
     it 'prints a board with X winning via the top row' do
@@ -67,7 +67,7 @@ describe "#display_board in 'lib/display_board.rb" do
       expect(output).to include("-----------")
       expect(output).to include("   |   |   ")
       expect(output).to include("-----------")
-      expect(output).to include("   |   |   ")           
+      expect(output).to include("   |   |   ")
     end
 
     it 'prints a board with O winning via the bottom row' do
@@ -79,7 +79,7 @@ describe "#display_board in 'lib/display_board.rb" do
       expect(output).to include("-----------")
       expect(output).to include("   |   |   ")
       expect(output).to include("-----------")
-      expect(output).to include(" O | O | O ")           
+      expect(output).to include(" O | O | O ")
     end
 
     it 'prints a board with X winning in a top left to bottom right diagonal' do
@@ -91,7 +91,7 @@ describe "#display_board in 'lib/display_board.rb" do
       expect(output).to include("-----------")
       expect(output).to include("   | X |   ")
       expect(output).to include("-----------")
-      expect(output).to include("   |   | X ")    
+      expect(output).to include("   |   | X ")
     end
 
     it 'prints a board with O winning in a top right to bottom left diagonal' do
@@ -103,7 +103,7 @@ describe "#display_board in 'lib/display_board.rb" do
       expect(output).to include("-----------")
       expect(output).to include("   | O |   ")
       expect(output).to include("-----------")
-      expect(output).to include(" O |   |   ")    
+      expect(output).to include(" O |   |   ")
     end
 
     it 'prints arbitrary arrangements of the board' do
@@ -115,7 +115,7 @@ describe "#display_board in 'lib/display_board.rb" do
       expect(output).to include("-----------")
       expect(output).to include(" X | O | O ")
       expect(output).to include("-----------")
-      expect(output).to include(" X | O | O ")          
+      expect(output).to include(" X | O | O ")
 
 
       board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
@@ -126,47 +126,58 @@ describe "#display_board in 'lib/display_board.rb" do
       expect(output).to include("-----------")
       expect(output).to include(" O | X | X ")
       expect(output).to include("-----------")
-      expect(output).to include(" O | X | O ")          
+      expect(output).to include(" O | X | O ")
     end
 
     it 'prints an entire board full of Xs' do
       # Should you want to write your own test for this situation,
-      # read the following code and comments and then comment out the following
-      # line by adding a # infront of skip.
-      skip("Can you copy the syntax of the tests above to write a test for a board entirely filled with Xs?")
+      # read the following code and comments.
+
+      # Can you copy the syntax of the tests above to write a test for a board
+      # entirely filled with Xs?"
 
       # Define the board with values that should create the desired output
       # *** Edit the line below ***
       board = [" ", " ", " ", " ", " ", " ", " ", " ", " "] # This is not correct
 
       # Don't touch the following line.
-      output = capture_puts{ display_board(board) }      
+      output = capture_puts{ display_board(board) } if defined?(display_board)
 
       # Each line that starts with expect represents a row in the ouput.
       # The desired characters a row must include are provided by the String
       # of the row. EX: The top row filled with X would be " X | X | X "
-      
+
       # You would code that expectation with:
       # expect(output).to include(" X | X | X ")
       # meaning you expect the entire output to at least include a matching row.
-      
 
-      # Edit the following lines to represent a board entirely filled with X.      
+      # Uncomment the following lines of code by removing the # at line start.
+      # Then edit the following lines to represent a board entirely filled with X.
       # Remember, every space and every character is important.
-      # *** Edit the lines below ***      
-      expect(output).to include("   |   |   ")
-      expect(output).to include("-----------")
-      expect(output).to include("   |   |   ")
-      expect(output).to include("-----------")
-      expect(output).to include("   |   |   ")          
+
+      # *** Edit the lines below ***
+      # *** Uncomment the lines below ***
+      # expect(output).to include("   |   |   ")
+      # expect(output).to include("-----------")
+      # expect(output).to include("   |   |   ")
+      # expect(output).to include("-----------")
+      # expect(output).to include("   |   |   ")
+
+      # *** Comment the line below by adding a # at the line start ***
+      expect(true).to be(true)
     end
 
     it 'prints an entire board full of Os' do
-      skip("Can you copy the syntax of the tests above to write a test for a board entirely filled with Os?")
+      # Can you copy the syntax of the tests above to write a test for a board
+      # entirely filled with Os?
+
       # Hint: You should be able to copy the code in the previous it example
       # and make a few simple edits to convert the previous example to this
       # example's situation.
       # Don't forget to comment out the line that begins with `skip`.
+
+      # *** Comment the line below by adding a # at the line start ***
+      expect(true).to be(true)
     end
   end
 end
