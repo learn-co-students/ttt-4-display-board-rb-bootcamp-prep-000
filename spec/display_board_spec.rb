7a7110057fa0324671ef0d75d8d3912f63bd695b
gabe <gj+code@mail.co.de>
2016-08-01 15:21:06 -0400

    'rows' was undefined in the bonus section (#26)

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -147,8 +147,9 @@ describe "#display_board in 'lib/display_board.rb" do
       # *** Edit the line below ***
       board = [" ", " ", " ", " ", " ", " ", " ", " ", " "] # This is not correct
 
-      # Don't touch the following line.
+      # Don't touch the following lines.
       output = capture_puts{ display_board(board) } if defined?(display_board)
+      rows = output.split("\n")
 
       # Each line that starts with expect represents a row in the ouput.
       # The desired characters a row must include are provided by the String
