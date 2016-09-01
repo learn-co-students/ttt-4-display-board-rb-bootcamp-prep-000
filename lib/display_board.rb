bfba6580aee2dbd01859aec47af33e97a4998697
Rey Tanada <reyvtanada@gmail.com>
2016-09-01 20:54:11 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,11 @@
 # out the current state.
 
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} \n-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} \n-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} \n"
+  row1 " #{board[0]} | #{board[1]} | #{board[2]} \n-----------"
+  row2 " #{board[3]} | #{board[4]} | #{board[5]} \n-----------"
+  row3 " #{board[6]} | #{board[7]} | #{board[8]} \n"
+
+  puts row1
+  puts row2
+  puts row3
 end
