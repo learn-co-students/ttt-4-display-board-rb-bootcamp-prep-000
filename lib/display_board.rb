7f3b66fd36ab9e0716d61c12f9abe54636aea450
David Morse <webdevdjm@gmail.com>
2016-09-04 14:07:48 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(array)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
