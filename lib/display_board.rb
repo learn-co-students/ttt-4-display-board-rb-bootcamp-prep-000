72b7c02f5fbb213a60fc97e4490484ab13aa03cd
Michael Heinrich <michael.heinrich@wavecable.com>
2016-09-21 04:21:49 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,18 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+# Define display_board that accepts a board and prints
+# out the current state.
+def display_board(rows)
+
+  pipes = '|'
+  dots = "-----------"
+
+  puts " #{rows[0]} #{pipes} #{rows[1]} #{pipes} #{rows[2]} "
+  puts "#{dots}"
+  puts " #{rows[3]} #{pipes} #{rows[4]} #{pipes} #{rows[5]} "
+  puts "#{dots}"
+  puts " #{rows[6]} #{pipes} #{rows[7]} #{pipes} #{rows[8]} "
+end
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+display_board(board)
