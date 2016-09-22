0a45dd7fd7d1f1ccf2a159e8132a8fd03710b2ad
Joel Cowie <joel.a.cowie@gmail.com>
2016-09-22 00:18:28 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,7 @@
 def display_board(board)
-  rows = [" #{board[0]} | #{board[1]} | #{board[2]} "],["-----------"],[" #{board[3]} | #{board[4]} | #{board[5]} "],["-----------"],[" #{board[6]} | #{board[7]} | #{board[8]} "]
-  puts rows
+  puts " #{board[0]} " + "|" + " #{board[1]} " + "|" + " #{board[2]} "
+  puts  "-----------"
+  puts  " #{board[3]} " + "|" + " #{board[4]} " + "|" + " #{board[5]} "
+  puts  "-----------"
+  puts  " #{board[6]} " + "|" + " #{board[7]} " + "|" + " #{board[8]} "
 end
