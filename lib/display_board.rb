0981ce480ecb1ec3931fbe1af0a9f36d2b698f8e
Danilo Carandang <jrcaranddang@gmail.com>
2016-09-27 03:57:21 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,21 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  rows = [
+    " #{board[0]} | #{board[1]} | #{board[2]} ",
+    "-----------",
+    " #{board[3]} | #{board[4]} | #{board[5]} ",
+    "-----------",
+    " #{board[6]} | #{board[7]} | #{board[8]} "]
+
+  puts rows[0]
+  puts rows[1]
+  puts rows[2]
+  puts rows[3]
+  puts rows[4]
+end
+
+#  row_one = " #{board[0]} | #{board[1]} | #{board[2]} "
+#  row_two = " #{board[4]} | #{board[5]} | #{board[6]} "
+#  row_three = " #{board[7]} | #{board[8]} | #{board[9]} "
+#  var bar = "-----------"
