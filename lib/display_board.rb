7269c49793ea35eb0c6c18dc9f60decb45e1a21a
Lauren Grapstein <lgrapstein@gmail.com>
2016-10-02 14:39:22 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,109 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+empty = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board(empty)
+  puts " #{empty[0]} | #{empty[1]} | #{empty[2]} "
+  puts "-----------"
+  puts " #{empty[3]} | #{empty[4]} | #{empty[5]} "
+  puts "-----------"
+  puts " #{empty[6]} | #{empty[7]} | #{empty[8]} "
+end
+
+situation_1 = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+
+def display_board(situation_1)
+  puts " #{situation_1[0]} | #{situation_1[1]} | #{situation_1[2]} "
+  puts "-----------"
+  puts " #{situation_1[3]} | #{situation_1[4]} | #{situation_1[5]} "
+  puts "-----------"
+  puts " #{situation_1[6]} | #{situation_1[7]} | #{situation_1[8]} "
+end
+
+situation_2 = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board(situation_2)
+  puts " #{situation_2[0]} | #{situation_2[1]} | #{situation_2[2]} "
+  puts "-----------"
+  puts " #{situation_2[3]} | #{situation_2[4]} | #{situation_2[5]} "
+  puts "-----------"
+  puts " #{situation_2[6]} | #{situation_2[7]} | #{situation_2[8]} "
+end
+
+situation_3 = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+
+def display_board(situation_3)
+  puts " #{situation_3[0]} | #{situation_3[1]} | #{situation_3[2]} "
+  puts "-----------"
+  puts " #{situation_3[3]} | #{situation_3[4]} | #{situation_3[5]} "
+  puts "-----------"
+  puts " #{situation_3[6]} | #{situation_3[7]} | #{situation_3[8]} "
+end
+
+situation_4 = ["X", "X", "X", " ", "O", " ", "O", " ", " "]
+
+def display_board(situation_4)
+  puts " #{situation_4[0]} | #{situation_4[1]} | #{situation_4[2]} "
+  puts "-----------"
+  puts " #{situation_4[3]} | #{situation_4[4]} | #{situation_4[5]} "
+  puts "-----------"
+  puts " #{situation_4[6]} | #{situation_4[7]} | #{situation_4[8]} "
+end
+
+situation_5 = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
+
+def display_board(situation_5)
+  puts " #{situation_5[0]} | #{situation_5[1]} | #{situation_5[2]} "
+  puts "-----------"
+  puts " #{situation_5[3]} | #{situation_5[4]} | #{situation_5[5]} "
+  puts "-----------"
+  puts " #{situation_5[6]} | #{situation_5[7]} | #{situation_5[8]} "
+end
+
+situation_6 = ["X", " ", " ", " ", "X", " ", "O", "O", "X"]
+
+def display_board(situation_6)
+  puts " #{situation_6[0]} | #{situation_6[1]} | #{situation_6[2]} "
+  puts "-----------"
+  puts " #{situation_6[3]} | #{situation_6[4]} | #{situation_6[5]} "
+  puts "-----------"
+  puts " #{situation_6[6]} | #{situation_6[7]} | #{situation_6[8]} "
+end
+
+situation_7 = ["X", " ", "O", " ", "O", " ", "O", " ", "X"]
+
+def display_board(situation_7)
+  puts " #{situation_7[0]} | #{situation_7[1]} | #{situation_7[2]} "
+  puts "-----------"
+  puts " #{situation_7[3]} | #{situation_7[4]} | #{situation_7[5]} "
+  puts "-----------"
+  puts " #{situation_7[6]} | #{situation_7[7]} | #{situation_7[8]} "
+end
+
+situation_8 = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
+
+def display_board(situation_8)
+  puts " #{situation_8[0]} | #{situation_8[1]} | #{situation_8[2]} "
+  puts "-----------"
+  puts " #{situation_8[3]} | #{situation_8[4]} | #{situation_8[5]} "
+  puts "-----------"
+  puts " #{situation_8[6]} | #{situation_8[7]} | #{situation_8[8]} "
+end
+
+situation_9 = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
+
+def display_board(situation_9)
+  puts " #{situation_9[0]} | #{situation_9[1]} | #{situation_9[2]} "
+  puts "-----------"
+  puts " #{situation_9[3]} | #{situation_9[4]} | #{situation_9[5]} "
+  puts "-----------"
+  puts " #{situation_9[6]} | #{situation_9[7]} | #{situation_9[8]} "
+end
+
+situation_10 = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+
+def display_board(situation_10)
+  puts " #{situation_10[0]} | #{situation_10[1]} | #{situation_10[2]} "
+  puts "-----------"
+  puts " #{situation_10[3]} | #{situation_10[4]} | #{situation_10[5]} "
+  puts "-----------"
+  puts " #{situation_10[6]} | #{situation_10[7]} | #{situation_10[8]} "
+end
