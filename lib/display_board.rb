3a75ae6a3bdfa00a9c0889f4260e29c956d716af
Darren Paddock <darrenlpaddock@gmail.com>
2016-10-14 19:58:38 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+
+
+def display_board(board)
+  puts row_1 = " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts separate = "-----------"
+  puts row_2 = " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts separate = "-----------"
+  puts row_2 = " #{board[6]} | #{board[7]} | #{board[8]} "
+end
