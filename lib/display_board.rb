f8bdf9cfd1fb041c7f252ce788af1dbb474291ca
Aleksandr Rogachev <aleksandr.rogachev1994@gmail.com>
2016-11-20 18:08:57 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,14 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(arr)
+
+  puts " #{arr[0]} | #{arr[1]} | #{arr[2] } "
+  print_div
+  puts " #{arr[3]} | #{arr[4]} | #{arr[5] } "
+  print_div
+  puts " #{arr[6]} | #{arr[7]} | #{arr[8] } "
+end
+
+def print_div
+  puts "-----------"
+end
