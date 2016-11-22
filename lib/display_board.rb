d7ded14343c7677fe8cdfbd596458d5b6067f330
Brett Brading <bbbrading@gmail.com>
2016-11-22 16:46:22 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  cell_row1 = " #{board[0]} | #{board[1]} | #{board[2]} "
+  cell_row2 = " #{board[3]} | #{board[4]} | #{board[5]} "
+  cell_row3 = " #{board[6]} | #{board[7]} | #{board[8]} "
+  sep_row = "-----------"
+  puts cell_row1
+  puts sep_row
+  puts cell_row2
+  puts sep_row
+  puts cell_row3
+end
