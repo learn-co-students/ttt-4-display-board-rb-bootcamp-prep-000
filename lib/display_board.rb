58b71db3208f862aac6599a3b4cd123fef2ddca3
Devon Weldon <devon.w.weldon@gmail.com>
2016-11-29 22:50:32 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(array)
+  print " #{array[0]} | #{array[1]} | #{array[2]} \n"
+  11.times{ print "-" }
+  puts
+  print print " #{array[3]} | #{array[4]} | #{array[5]} \n"
+  11.times{ print "-" }
+  puts
+  print print " #{array[6]} | #{array[7]} | #{array[8]} \n"
+end 
