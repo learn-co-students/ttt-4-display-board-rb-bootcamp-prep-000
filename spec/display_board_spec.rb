4290b27172d9e241246b258e8229741735ad2ee6
Devon Weldon <devon.w.weldon@gmail.com>
2016-11-30 00:17:06 +0000

    Done.

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -145,7 +145,7 @@ describe "#display_board in 'lib/display_board.rb" do
 
       # Define the board with values that should create the desired output
       # *** Edit the line below ***
-      board = [" ", " ", " ", " ", " ", " ", " ", " ", " "] # This is not correct
+      board = ["x", "x", "x", "x", "x", "x", "x", "x", "x"] # This is not correct
 
       # Don't touch the following lines.
       output = capture_puts{ display_board(board) } if defined?(display_board)
@@ -165,14 +165,14 @@ describe "#display_board in 'lib/display_board.rb" do
 
       # *** Edit the lines below ***
       # *** Uncomment the lines below ***
-      # expect(rows[0]).to eq("   |   |   ")
-      # expect(rows[1]).to eq("-----------")
-      # expect(rows[2]).to eq("   |   |   ")
-      # expect(rows[3]).to eq("-----------")
-      # expect(rows[4]).to eq("   |   |   ")
+       expect(rows[0]).to eq(" x | x | x ")
+       expect(rows[1]).to eq("-----------")
+       expect(rows[2]).to eq(" x | x | x ")
+       expect(rows[3]).to eq("-----------")
+       expect(rows[4]).to eq(" x | x | x ")
 
       # *** Comment the line below by adding a # at the line start ***
-      expect(true).to be(true)
+      #expect(true).to be(true)
     end
 
     it 'prints an entire board full of Os' do
@@ -182,9 +182,22 @@ describe "#display_board in 'lib/display_board.rb" do
       # Hint: You should be able to copy the code in the previous it example
       # and make a few simple edits to convert the previous example to this
       # example's situation.
+      board = ["o", "o", "o", "o", "o", "o", "o", "o", "o"] 
+
+      # Don't touch the following lines.
+      output = capture_puts{ display_board(board) } if defined?(display_board)
+      rows = output.split("\n")
+
+
+
+       expect(rows[0]).to eq(" o | o | o ")
+       expect(rows[1]).to eq("-----------")
+       expect(rows[2]).to eq(" o | o | o ")
+       expect(rows[3]).to eq("-----------")
+       expect(rows[4]).to eq(" o | o | o ")
 
       # *** Comment the line below by adding a # at the line start ***
-      expect(true).to be(true)
+      #expect(true).to be(true)
     end
   end
 end
