e43910329b97a1d94d5c7b44f543917d8f8659c1
Pak Belalang <opblang@yandex.com>
2016-12-02 21:01:18 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board=[])
+  template = [
+    " #{board[0]} ","|"," #{board[1]} ","|"," #{board[2]} ", "\n",
+    "-----------", "\n",
+    " #{board[3]} ","|"," #{board[4]} ","|"," #{board[5]} ", "\n",
+    "-----------", "\n",
+    " #{board[6]} ","|"," #{board[7]} ","|"," #{board[8]} ", "\n"
+  ]
+  template.each { |x| print x }
+end
