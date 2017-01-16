bef1178f0f822ef1ae1ccdc3d1a16b46a7e4b013
Malki Davis <md01@me.com>
2017-01-16 22:06:15 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board
-puts " #{" "} | #{" "} | #{" "} "
+def display_board(letter)
+puts " #{letter[0]} | #{letter[1]} | #{letter[2]} "
 puts "-----------"
-puts " #{" "} | #{" "} | #{" "} "
+puts " #{letter[3]} | #{letter[4]} | #{letter[5]} "
 puts "-----------"
-puts " #{" "} | #{" "} | #{" "} "
+puts " #{letter[6]} | #{letter[7]} | #{letter[8]} "
 end
