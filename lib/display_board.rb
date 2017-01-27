68697c286f4225ba81934e112a1d2d7962f0c76c
Andrew Zheng <andrew.zheng725@gmail.com>
2017-01-27 03:02:17 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,14 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  row1 = " #{board[0]} | #{board[1]} | #{board[2]} "
+  row2 = " #{board[3]} | #{board[4]} | #{board[5]} "
+  row3 = " #{board[6]} | #{board[7]} | #{board[8]} "
+  divide = "-----------"
+  puts row1
+  puts divide
+  puts row2
+  puts divide
+  puts row3
+end
+  
