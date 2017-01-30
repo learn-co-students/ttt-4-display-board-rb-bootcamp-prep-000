3bfe11c2813345d5279e98fbe07b1ef841f2ffca
Nadine Fischoff <nadine.khattak@gmail.com>
2017-01-30 16:47:51 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 def display_board(board)
   first_row = " #{board[0]} | #{board[1]} | #{board[2]} "
-  line_row = "-----------"
+  line_row = "-----------"cd labs
   second_row = " #{board[3]} | #{board[4]} | #{board[5]} "
   third_row = " #{board[6]} | #{board[7]} | #{board[8]} "
   puts first_row + "\n" + line_row + "\n" + second_row + "\n" + line_row + "\n" + third_row
