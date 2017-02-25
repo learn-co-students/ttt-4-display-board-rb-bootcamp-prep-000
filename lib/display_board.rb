6d45d52af6b2898eeb6a6bba0e22595c6762216d
Matteo Piccini <gnappo.1@live.it>
2017-02-25 02:25:32 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,14 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(board)
+  puts  " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} "
+end
+display_board([" "," "," "," "," "," "," "," "," "])
+display_board([" "," "," "," ","X"," "," "," "," "])
+display_board(["O"," "," "," "," "," "," "," "," "])
+display_board(["O"," "," "," ","X"," "," "," "," "])
+display_board(["X","X","X"," "," "," "," "," "," "])
+display_board([" "," "," "," "," "," ","O","O","O"])
+display_board(["X"," "," "," ","X"," "," "," ","X"])
+display_board([" "," ","O"," ","O"," ","O"," "," "])
+display_board(["X","X","X","X","O","O","X","O","O"])
+display_board(["X","X","X","X","X","X","X","X","X"])
+display_board(["O","O","O","O","O","O","O","O","O"])
