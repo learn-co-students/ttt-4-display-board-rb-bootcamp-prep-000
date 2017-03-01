c6d8025da0d9bd4ee8855d19907708c211e0da29
Chirantan Ghosh <chrn.ghosh@gmail.com>
2017-03-01 05:01:45 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,21 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  cell = "   "
+  separator = "|"
+  line = "-----------"
+  output = ""
+  i = 0
+  while i < board.length do
+    output += cell.replace(" #{board[i]} ")
+    if i == 2 || i == 5
+      output += "\n#{line}\n"
+    elsif i == 8
+      output += "\n"
+    else
+      output += "#{separator}"
+    end
+    i += 1
+  end
+  puts output
+end
