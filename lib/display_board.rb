624d813d65e4bf051b9cf89e938c630f7b299574
Tony Distinti <adistinti@gmail.com>
2017-03-20 18:15:12 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,11 +2,13 @@
 # out the current state.
 
  def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} 
+  puts " #{board[0]} | #{board[1]} | #{board[2]}
 -----------
- #{board[3]} | #{board[4]} | #{board[5]} 
+ #{board[3]} | #{board[4]} | #{board[5]}
 -----------
  #{board[6]} | #{board[7]} | #{board[8]} "
+
+## Note that the stuff below did not work
 #  puts "-----------"
 #  puts " #{board[3]}  | #{board[4]} | #{board[5]} "
 #  puts "-----------"
