5ed2b240c8684deaa64c89a5d434913f4edba27a
Nancy Noyes <nnoyes@comcast.net>
2017-04-18 16:59:00 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board (moves)
-  puts " #{moves[0]} | #{moves[1]} | #{moves[2]}"
+  puts " #{moves[0]} | #{moves[1]} | #{moves[2]} "
   puts "-----------"
-  puts " #{moves[3]} | #{moves[4]} | #{moves[5]}"
+  puts " #{moves[3]} | #{moves[4]} | #{moves[5]} "
   puts "-----------"
-  puts " #{moves[6]} | #{moves[7]} | #{moves[8]}"
+  puts " #{moves[6]} | #{moves[7]} | #{moves[8]} "
 end
