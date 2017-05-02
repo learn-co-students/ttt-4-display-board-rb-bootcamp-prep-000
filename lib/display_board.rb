49a052e112f15edb9de84e8dd6af4f1abc2e914a
Aurangzaib Danial Liaqat Khan <aurangzaib.danial@gmail.com>
2017-05-02 08:35:22 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+print " #{board[0]} | #{board[1]} | "
+puts "#{board[2]} "
+puts "-----------"
+print " #{board[3]} | #{board[4]} | "
+puts "#{board[5]} "
+puts "-----------"
+print " #{board[6]} | #{board[7]} | "
+puts "#{board[8]} "
+end
