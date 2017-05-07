1754d9350297932d1d5aa615129f24275f975807
Tyler Kotlowski <kotlowski.tyler@gmail.com>
2017-05-07 17:43:18 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,35 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(board)
+  #Interpolation from board array
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+puts "--------------------------"
+puts "| Welcome To Tic Tac Toe! |" #Intro to game
+puts "--------------------------"
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "] #Blank board
+display_board(board)
+
+puts "---------"
+puts '|Move 1!|'
+puts "---------"
+
+board = ["O", " ", " ", " ", "X", " ", " ", " ", " "] #First move
+display_board(board) #Output of game
+
+puts "---------"
+puts '|Move 2!|'
+puts "---------"
+
+board = [" ", " ", " ", " ", "X", " ", " ", " ", "O"] #Second move
+display_board(board) #Output of game
+
+puts "---------"
+puts '|Move 3!|'
+puts "---------"
+
+board = [" ", "X", " ", " ", "X", " ", " ", " ", "O"]
+display_board(board)
