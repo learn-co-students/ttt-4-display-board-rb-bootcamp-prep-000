d4aeffdaa91620c9d9781eba322140d1dc3fd61e
Sean Denison <sean.denison26@gmail.com>
2017-05-10 23:33:57 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
-# Define display_board that accepts a board and prints
+# display_board that accepts a board and prints
 # out the current state.
+def display_board(b)
+  row = "-----------"
+  col = "|"
+  def cell(space)
+    return " #{space} "
+  end
+  display = cell(b[0]) + col + cell(b[1]) + col + cell(b[2]) + "\n" + row + "\n" + cell(b[3]) + col + cell(b[4]) + col + cell(b[5]) + "\n" + row  +  "\n" + cell(b[6]) + col + cell(b[7]) + col + cell(b[8])
+  puts display
+end
