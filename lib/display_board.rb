2f6a1671e42031f9ef53047c2349868ee8a19dc6
luke Winkelmann <lgwink89@yahoo.com>
2017-05-13 14:59:16 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(spot)
+  puts " #{spot[0]} | #{spot[1]} | #{spot[2]} "
+  puts "-----------"
+  puts " #{spot[3]} | #{spot[4]} | #{spot[5]} "
+  puts "-----------"
+  puts " #{spot[6]} | #{spot[7]} | #{spot[8]} "
+end
