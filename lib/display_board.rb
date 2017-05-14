fa57a8429d54cc296b31b74d0d79162607a4385f
Jeremy <emata@rapidratings.com>
2017-05-13 21:50:23 -0400

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,9 @@
-# Define display_board that accepts a board and prints
-# out the current state.
-board=["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
 
-def display_board(board)
-puts " #{board[0]} | #{board[1]} | #{board[2]} "
-puts "-----------"
-puts " #{board[3]} | #{board[4]} | #{board[5]} "
-puts "-----------"
-puts " #{board[6]} | #{board[7]} | #{board[8]} "
+
+def display_board(array)
+  puts " #{array[0]} | #{array[1]} | #{array[2]} "
+  puts "-----------"
+  puts " #{array[3]} | #{array[4]} | #{array[5]} "
+  puts "-----------"
+  puts " #{array[6]} | #{array[7]} | #{array[8]} "
 end
