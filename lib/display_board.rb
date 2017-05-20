058f5c3dea9cfe640f7bfbc26a473f55f08ae382
Robert Gonzalez <robertgonzalez@utexas.edu>
2017-05-20 21:33:00 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,16 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+#  while i < 8
+#  if board[0]
+#row1 = " #{board[0]} | #{board[1]} | #{board[2]} "
+#row2 = "-----------"
+#row3 = " #{board[3]} | #{board[4]} | #{board[5]} "
+#row4 = row 2
+#row5 = " #{board[6]} | #{board[7]} | #{board[8]} "
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts"-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
