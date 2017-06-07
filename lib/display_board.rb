d337220c8219eb0754a632ea54f38ab31791d454
Nathan  Visak <nathan.visak@gmail.com>
2017-06-07 15:51:08 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,15 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def display_board (board)
+  first_row = " #{board [0]} | #{board [1]} | #{board [2]} "
+  puts first_row
+  divider_line = "-----------"
+  puts divider_line
+  second_row = " #{board [3]} | #{board [4]} | #{board [5]} "
+  puts second_row
+  puts divider_line
+  third_row = " #{board [6]} | #{board [7]} | #{board [8]} "
+  puts third_row
+end
+display_board (board)
