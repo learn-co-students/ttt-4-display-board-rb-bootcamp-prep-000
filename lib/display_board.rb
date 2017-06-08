94c52352bb21bed550110ab3a516c9161b8556c8
Taurean Branch <tdbranchii@gmail.com>
2017-06-08 19:45:58 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,15 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  row0 =[ " #{board[0]} ","|"," #{board[1]} ","|"," #{board[2]} "]
+  row1 =["-----------"]
+  row2 =[ " #{board[3]} ","|"," #{board[4]} ","|"," #{board[5]} "]
+  row3 =[ "-----------"]
+  row4 =[ " #{board[6]} ","|"," #{board[7]} ","|"," #{board[8]} "]
+
+  puts row0.join
+  puts row1
+  puts row2.join
+  puts row3
+  puts row4.join
+end
