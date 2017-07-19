95f06d6b640474ab4cab8dda31b05c8222b214ec
Nick Cianci <n.cianci3@gmail.com>
2017-07-19 21:50:06 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,8 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(board)
+  rows = board.length / 3
+  rows.times do |row_idx|
+    offset_idx = row_idx * rows
+    puts " #{board[offset_idx]} | #{board[offset_idx + 1]} | #{board[offset_idx + 2]} "
+    puts "-" * ((rows * 4) - 1) unless row_idx == rows - 1
+  end
+end
