e5e51d0c5d6ada8adc68da37c9392c4f5f4e5d33
Kevin Huang <yellow.khaos@gmail.com>
2017-07-25 04:12:14 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,15 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def printDash()
+  puts "-----------"
+end
+
+def printEmpty(x,y,z)
+  puts " #{x} | #{y} | #{z} "
+end
+
+def display_board(board)
+  printEmpty(board[0],board[1],board[2])
+  printDash()
+  printEmpty(board[3],board[4],board[5])
+  printDash()
+  printEmpty(board[6],board[7],board[8])
+end
