635eda403a15cb10dc335a6d9e33b39165ec4414
Joey Cho <cjy12182000@naver.com>
2017-07-27 18:05:04 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,19 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board(board)
+
+1.times {puts(" #{board[0]} "+"|"+" #{board[1]} "+"|"+" #{board[2]} ")}
+#1.times {puts("   "+" "+"   "+" "+"   ")}
+
+1.times {puts("-----------")}
+#1.times {puts("   "+" "+"   "+" "+"   ")}
+1.times {puts(" #{board[3]} "+"|"+" #{board[4]} "+"|"+" #{board[5]} ")}
+#1.times {puts("   "+" "+"   "+" "+"   ")}
+1.times {puts("-----------")}
+
+#1.times {puts("   "+" "+"   "+" "+"   ")}
+1.times {puts(" #{board[6]} "+"|"+" #{board[7]} "+"|"+" #{board[8]} ")}
+
+
+end
