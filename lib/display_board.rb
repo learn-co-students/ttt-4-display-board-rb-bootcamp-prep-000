edffe8e4fa093db65f6220b22dcd168069a69758
Tyler Williams <badtoy1986@gmail.com>
2017-08-02 08:12:36 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(board)
+  seperator = "-----------"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts seperator
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts seperator
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
