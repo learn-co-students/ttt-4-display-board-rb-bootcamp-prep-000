fae3b8d27cc82e8560adaf712b95de97f7df10c1
0000000 00000000 <xninfo2015@gmail.com>
2017-08-04 09:40:22 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(board)
+  b = []
+  (0...board.size-2).step(3) { |i|  b << board[i..i+2].map {|letter| " #{letter} "}.join("|")}
+  sep = "-" * b[0].size
+  puts b.join("\n" + sep + "\n")
+end
+
+
+display_board( ["O", "X", " ", " ", "X", " ", "X", "O", " "])
