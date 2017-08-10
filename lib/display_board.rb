3eeb201693571d03c9720991ba55f89de353fede
Diego Flores <dflores@fandm.edu>
2017-08-10 21:24:01 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,8 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(board)
+  firstRow = [" #{board[0]} " + "| #{board[1]} |" + " #{board[2]} "]
+  secondRow = ["-----------"]
+  thirdRow = [" #{board[3]} " + "| #{board[4]} |" + " #{board[5]} "]
+  fourthRow = ["-----------"]
+  fifthRow = [" #{board[6]} " + "| #{board[7]} |" + " #{board[8]} "]
+  puts firstRow, secondRow, thirdRow, fourthRow, fifthRow
+end
