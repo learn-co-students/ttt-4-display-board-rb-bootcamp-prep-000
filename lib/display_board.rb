4214c9eb8eee68ec9081d6f6ea3619cff81c2596
George gore9213@gmail.com <georgelgore@gmail.com>
2017-08-17 22:05:00 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(array)
+ board = " #{array[0]} | #{array[1]} | #{array[2]} \n-----------\n #{array[3]} | #{array[4]} | #{array[5]} \n-----------\n #{array[6]} | #{array[7]} | #{array[8]} \n"
+ puts board
+end
