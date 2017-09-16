1f8c0d976da56b5985ffa01564a3315b735059d8
Anthony Fields <janthonyfields@gmail.com>
2017-09-16 06:14:13 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  horiz = "-----------"
+
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "#{horiz}"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "#{horiz}"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
