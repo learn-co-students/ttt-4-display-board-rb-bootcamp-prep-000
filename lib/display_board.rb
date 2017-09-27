966105c27c5007dc0a1c6de9100d7e5774754d8a
Anthony Grimes <agrimes@charter.net>
2017-09-27 17:36:39 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(options)
+    puts " #{options[0]} | #{options[1]} | #{options[2]} "
+    puts "-----------"
+    puts " #{options[3]} | #{options[4]} | #{options[5]} "
+    puts "-----------"
+    puts " #{options[6]} | #{options[7]} | #{options[8]} "
+end
