09ddf2ab405ab41f9337db93a3b55b4b6dc96d6a
Blessing Ebowe <blessingebowe@gmail.com>
2017-10-19 11:43:19 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,20 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def divider
+  "-----------\n"
+end
+
+def box
+  "   |   |   \n"
+end
+
+def display_board(board)
+  box_postions_for_O_or_X = [1, 5, 9, 13, 17, 21, 25, 29, 33]
+  str = box + box + box
+  box_postions_for_O_or_X.each_with_index do |val, ind|
+    if ((board[ind] != " ") & (board[ind] != ""))
+      str[val] = board[ind]
+    end
+  end
+  str[11] = "\n#{divider}"
+  str[35] = "\n#{divider}"
+  puts str
+end
