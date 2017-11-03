0b0d6c2761e5a267a2a2289f284aba6a46a8e647
Christopher Diep <diep.christopher@gmail.com>
2017-11-03 01:32:56 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  vertical_space = "   |   |   "
+  horizontal_space = "-----------"
+  puts vertical_space
+  puts horizontal_space
+  puts vertical_space
+  puts horizontal_space
+  puts vertical_space
+end
