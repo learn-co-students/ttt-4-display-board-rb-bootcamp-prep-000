46d594ed505afdbc647b8025f92d0892181fb05a
Kevin Randles <krandles@gmail.com>
2017-11-04 02:57:37 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,6 +2,14 @@
 # out the current state.
 board = [" "," "," "," "," "," "," "," "," "]
 
-def display_board
-
+def display_board(board)
+  result = ""
+  divider = "-----------\n"
+  result << " #{board[0]} | #{board[1]} | #{board[2]} \n"
+  result << divider
+  result << " #{board[3]} | #{board[4]} | #{board[5]} \n"
+  result << divider
+  result << " #{board[6]} | #{board[7]} | #{board[8]} \n"
+  print result
 end
+display_board(board)
