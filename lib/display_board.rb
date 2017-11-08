c7db9b180b5bb5bb622b53651ee3601938823f68
Misun Son <misun.son1022@gmail.com>
2017-11-08 21:27:06 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,17 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+
+  rows = []
+
+  rows[0] = " #{board[0]} | #{board[1]} | #{board[2]} "
+  rows[1] = " #{board[3]} | #{board[4]} | #{board[5]} "
+  rows[2] = " #{board[6]} | #{board[7]} | #{board[8]} "
+
+  rows.each_with_index do |row, i|
+    puts row
+    if i!=(rows.length-1)
+      puts "-----------"
+    end
+  end
+end
