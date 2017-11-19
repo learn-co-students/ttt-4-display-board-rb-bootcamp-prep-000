1cc38938d20614d7e974f33a99008c90d8ec5190
James Gary <bucket@earthlink.net>
2017-11-19 19:30:53 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,13 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(board)
+  board_temp = []
+  board.each do |x|
+    board_temp << " "+x+" "
+  end
+
+  puts  board_temp[0] + "|" + board_temp[1] + "|"+ board_temp[2]
+  puts "-----------"
+  puts  board_temp[3] + "|" + board_temp[4] + "|"+ board_temp[5]
+  puts "-----------"
+  puts  board_temp[6] + "|" + board_temp[7] + "|"+ board_temp[8]
+
+end
