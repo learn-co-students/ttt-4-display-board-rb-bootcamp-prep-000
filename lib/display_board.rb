04c8b58e8f333d104ac8958b5300dc8624c2d8b1
Kathryn La Tour <klatour324@gmail.com>
2017-11-27 21:50:33 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
   def display_board(board)
-    blank_board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    # blank_board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
     puts '-----------'
     puts " #{board[3]} | #{board[4]} | #{board[5]} "
