3e86cfb213295113c7b30103a1056dfe07f6a5c5
Jacob Denbeaux <jdenbeaux19@wooster.edu>
2017-12-09 09:41:11 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def row a, b, c
+  puts " #{a} | #{b} | #{c} "
+end
+
+def display_board board
+  row board[0], board[1], board[2]
+  puts '-----------'
+  row board[3], board[4], board[5]
+  puts '-----------'
+  row board[6], board[7], board[8]
+end
