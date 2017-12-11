6bd7dc82e9be4eee6be9e3e390cf2ebc6aa8a320
Kimberly Mozzetti <kimberly.mozzetti@gmail.com>
2017-12-11 12:21:31 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,17 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  cell_divider = "|"
+  row_divider = "-----------"
+
+  row1 = " #{board[0]} #{cell_divider} #{board[1]} #{cell_divider} #{board[2]} "
+  row2 = " #{board[3]} #{cell_divider} #{board[4]} #{cell_divider} #{board[5]} "
+  row3 = " #{board[6]} #{cell_divider} #{board[7]} #{cell_divider} #{board[8]} "
+
+  puts row1
+  puts row_divider
+  puts row2
+  puts row_divider
+  puts row3
+
+end
