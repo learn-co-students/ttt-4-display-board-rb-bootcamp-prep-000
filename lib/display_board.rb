0713d0506e5a1d5420bcbacd4225454be21c0b1f
Stephane <son.trinh.dev@gmail.com>
2017-12-23 02:27:52 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,14 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(board)
+  row1 = board[0..2].join(' | ')
+  row2 = board[3..5].join(' | ')
+  row3 = board[6..8].join(' | ')
+  puts " #{row1} "
+  puts '-' * 11
+  puts " #{row2} "
+  puts '-' * 11
+  puts " #{row3} "
+
+end
