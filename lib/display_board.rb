7d221209001689830b02a11061dcb6868d22ca57
A I <atbisrael@gmail.com>
2018-01-03 20:40:39 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,28 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+ board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
+
+def display_board(board)
+ puts " #{board[0]} | #{board[1]} | #{board[2]} "
+ puts "-----------"
+ puts " #{board[3]} | #{board[4]} | #{board[5]} " 
+ puts "-----------"
+ puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# def display_board(board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "])
+#   puts "   " + "|" + "   " + "|" + "   "
+#   puts "-----------"
+#   puts "   " + "|" + "   " + "|" + "   "
+#   puts "-----------"
+#   puts "   " + "|" + "   " + "|" + "   "
+# end
+# board1 = ["   ", "   ", "   ", "   ", "X", "   ", "   ", "   ", "   "]
+# display_board(board1)   
+# board2 = ["O", "   ", "   ", "   ", "X", "   ", "   ", "   ", "   "]
+# display_board(board2)   
+# board3 = ["X", "X", "X", "   ", "   ", "   ", "   ", "   ", "   "]
+# display_board(board3)
+# board4 = ["   ", "   ", "   ", "   ", "   ", "   ", "O", "O", "O"]
+# board5 = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
