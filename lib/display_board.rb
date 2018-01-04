db7046c0735c03f2837e69e49fa9bee7c75a4306
Jaime Zuspann <jaimeadele@gmail.com>
2018-01-04 18:33:56 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,16 +6,29 @@ def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def x_turn
-  print "Enter a number from 1 to 9: "
+def x_turn(board)
+  get_move(board, "X")
+  display_board(board)
 end
 
-def o_turn
-
+def o_turn(board)
+  get_move(board, "O")
+  display_board(board)
 end
 
-def play_game
-  x_turn()
+def get_move(board, player)
+  print "Enter a number from 1-9: "
+  move = gets.chomp.to_i - 1
+  while board[move] == "X" || board[move] == "O" || move > 8
+    print "Enter a different number: "
+    move = gets.chomp.to_i - 1
+  end
+  board[move] = player
 end
 
-play_game
+def play_game(board)
+  x_turn(board)
+  o_turn(board)
+end
+
+play_game(board)
