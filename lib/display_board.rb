0fced20cd75ffc4007ce928c393394b1ae1de0f0
lsiler23 <siler.laurette@gmail.com>
2018-01-04 17:18:55 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,14 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  
+  row0 = board[0..2].join(" | ")
+  row1 = board[3..5].join(" | ")
+  row2 = board[6..8].join(" | ")
+  
+  puts " " + row0 + " "
+  puts "-----------"
+  puts " " + row1 + " "
+  puts "-----------"
+  puts " " + row2 + " "
+end 
