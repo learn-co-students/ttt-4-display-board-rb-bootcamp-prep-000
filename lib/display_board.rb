c0af559099e55982c2ef8e4c20b13e53d8c66afb
Jaime Zuspann <jaimeadele@gmail.com>
2018-01-04 18:11:44 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,21 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def x_turn
+  print "Enter a number from 1 to 9: "
+end
+
+def o_turn
+
+end
+
+def play_game
+  x_turn()
+end
+
+play_game
