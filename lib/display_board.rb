5950ba70fba134f244044f8aee52b6075cf25cfb
Micah Shute <micah.shute@gmail.com>
2018-01-28 04:39:30 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,34 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+$moveArr = []
+$colDivider = "|"
+$rowDivider = "-----------"
+
+def dispLocation(move)
+  print " #{$moveArr[move]} "
+end
+
+def display_board(board = initializeVal)
+  $moveArr = board;
+  numOfRows = 2
+  numOfCols = 2
+  for i in 0..numOfRows do
+    for j in 0..numOfCols do
+      dispLocation(i*3 + j)
+      if j < numOfRows
+        print $colDivider
+      else
+        puts ""
+      end
+    end
+    if i < numOfCols
+      puts $rowDivider
+    end
+  end
+end
+
+def initializeVal
+  initVal = " "
+  for i in 0..8 do
+    $moveArr << initVal
+  end
+  return $moveArr
+end
