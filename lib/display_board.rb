73e27ab2aa50368b3ab82d6437ac72b2a310bf7b
Peter Coughlin <coughlin1966@gmail.com>
2018-01-28 16:22:17 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,15 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  row1 = "   |   |   "
+  row2 = "   |   |   "
+  row3 = "   |   |   "
+  column1 = "-----------"
+  column2 = "-----------"
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  puts row1
+  puts column1
+  puts row2
+  puts column2
+  puts row3
+  end
