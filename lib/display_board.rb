914e3bbb62a91051409fcfa6a2bae4ac0734308b
Teresa Terry <dr.terry2013@gmail.com>
2018-02-07 15:19:30 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,14 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
+
+def display_board(arr)
+  pipe = "|"
+  sep_line = "-"*11
+  puts " #{arr[0]} #{pipe} #{arr[1]} #{pipe} #{arr[2]} "
+  puts "#{sep_line}"
+  puts " #{arr[3]} #{pipe} #{arr[4]} #{pipe} #{arr[5]} "
+  puts "#{sep_line}"
+  puts " #{arr[6]} #{pipe} #{arr[7]} #{pipe} #{arr[8]} "
+end
