6470a78631a5484458e54b0f1127c14e441d4e92
Kevin Li <kevinjcli@hotmail.com>
2018-02-23 04:27:06 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+LINE = "-----------"
+
+def display_board (tic_or_tac = [" ", " ", " ", " ", " ", " ", " ", " ", " ",])
+  
+  puts " #{tic_or_tac[0]} | #{tic_or_tac[1]} | #{tic_or_tac[2]} "
+  puts LINE
+  puts " #{tic_or_tac[3]} | #{tic_or_tac[4]} | #{tic_or_tac[5]} "
+  puts LINE
+  puts " #{tic_or_tac[6]} | #{tic_or_tac[7]} | #{tic_or_tac[8]} "
+end
