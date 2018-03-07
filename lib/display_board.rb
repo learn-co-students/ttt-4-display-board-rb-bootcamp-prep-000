f65131bdb007d21b1505690fb2ccc3fc25ab488b
sheesa azeeleeza <leezaluncheon@gmail.com>
2018-03-07 00:06:34 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(cell)
+  puts " #{cell[0]} | #{cell[1]} | #{cell[2]} "
+  puts "-----------"
+  puts " #{cell[3]} | #{cell[4]} | #{cell[5]} "
+  puts "-----------"
+  puts " #{cell[6]} | #{cell[7]} | #{cell[8]} "
+end
