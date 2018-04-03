2525958368a6dae6a169acdd40c9cfe79607a0ce
ERIC KELLY <eakelly220@gmail.com>
2018-04-03 22:35:23 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(map)
+  puts" #{map[0]} | #{map[1]} | #{map[2]} 
+-----------
+ #{map[3]} | #{map[4]} | #{map[5]} 
+-----------
+ #{map[6]} | #{map[7]} | #{map[8]} "
+  
+  
+end
+
+
