37e8b1aa383d998d83148dfc7a89f4f1e11f86b0
John Gratton <jgratton@gmail.com>
2018-04-06 02:04:31 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = Array.new(9, " ")
+
+def display_board board
+  dashes = "-----------"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts dashes
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts dashes
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
