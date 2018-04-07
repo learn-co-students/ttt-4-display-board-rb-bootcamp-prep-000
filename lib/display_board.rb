88933ddf648341a5de0c014f2d183a0110a46a2f
Ryan Patel <rajanpatel1011@gmail.com>
2018-04-07 00:01:15 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(game)
+  print "#{game[0]} | #{game[1]} | #{game[2]}"
+  print "-----------"
+  print "#{game[3]} | #{game[4]} | #{game[5]}"
+  print "-----------"
+  print "#{game[6]} | #{game[7]} | #{game[8]}"
+end
