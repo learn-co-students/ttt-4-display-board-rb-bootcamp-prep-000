7cd561d4a844498377225af2ec7401400e742d0c
Jathmel Gordon <jathmel@gmail.com>
2018-04-13 17:56:25 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,6 +5,6 @@ def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]}"
   puts "----------------------------------------"
   puts " #{board[3]} | #{board[4]} | #{board[5]}"
-   puts "----------------------------------------"
+  puts "----------------------------------------"
   puts " #{board[6]} | #{board[7]} | #{board[8]}"
 end
