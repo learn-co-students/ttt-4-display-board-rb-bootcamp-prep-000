b5facaa06e4e5e62906be20508b1680f6137ccf7
Wasif Khan <waasif.khaan@gmail.com>
2018-04-20 05:49:21 +0000

    Done.

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -137,6 +137,17 @@ describe "#display_board in 'lib/display_board.rb" do
     end
 
     it 'prints an entire board full of Xs' do
+      board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
+
+      output = capture_puts{ display_board(board) }
+      rows = output.split("\n")
+
+      expect(rows[0]).to eq(" X | X | X ")
+      expect(rows[1]).to eq("-----------")
+      expect(rows[2]).to eq(" X | X | X ")
+      expect(rows[3]).to eq("-----------")
+      expect(rows[4]).to eq(" X | X | X ")
+    end
       # Should you want to write your own test for this situation,
       # read the following code and comments.
 
@@ -145,11 +156,11 @@ describe "#display_board in 'lib/display_board.rb" do
 
       # Define the board with values that should create the desired output
       # *** Edit the line below ***
-      board = [" ", " ", " ", " ", " ", " ", " ", " ", " "] # This is not correct
+      #board = [" ", " ", " ", " ", " ", " ", " ", " ", " "] # This is not correct
 
       # Don't touch the following lines.
-      output = capture_puts{ display_board(board) } if defined?(display_board)
-      rows = output.split("\n")
+      #output = capture_puts{ display_board(board) } if defined?(display_board)
+      #rows = output.split("\n")
 
       # Each line that starts with expect represents a row in the ouput.
       # The desired characters a row must include are provided by the String
@@ -172,10 +183,21 @@ describe "#display_board in 'lib/display_board.rb" do
       # expect(rows[4]).to eq("   |   |   ")
 
       # *** Comment the line below by adding a # at the line start ***
-      expect(true).to be(true)
-    end
+      #expect(true).to be(true)
+
 
     it 'prints an entire board full of Os' do
+      board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+
+      output = capture_puts{ display_board(board) }
+      rows = output.split("\n")
+
+      expect(rows[0]).to eq(" O | O | O ")
+      expect(rows[1]).to eq("-----------")
+      expect(rows[2]).to eq(" O | O | O ")
+      expect(rows[3]).to eq("-----------")
+      expect(rows[4]).to eq(" O | O | O ")
+end
       # Can you copy the syntax of the tests above to write a test for a board
       # entirely filled with Os?
 
@@ -184,7 +206,7 @@ describe "#display_board in 'lib/display_board.rb" do
       # example's situation.
 
       # *** Comment the line below by adding a # at the line start ***
-      expect(true).to be(true)
-    end
+     #expect(true).to be(true)
+
   end
 end
