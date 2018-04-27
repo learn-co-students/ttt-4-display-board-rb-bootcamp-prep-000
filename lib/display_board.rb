3b342db623ec89c8012f358cbcac0418a94606fd
Jigar Polra <jigarpolra@live.co.uk>
2018-04-27 12:35:55 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,16 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  boardLineTop = " #{board[0]} | #{board[1]} | #{board[2]} "
+  boardLineMiddle = " #{board[3]} | #{board[4]} | #{board[5]} "
+  boardLineBottom = " #{board[6]} | #{board[7]} | #{board[8]} "
+  boardGap = ""
+  11.times { boardGap += "-" }
+
+  puts boardLineTop;
+  puts boardGap;
+  puts boardLineMiddle;
+  puts boardGap;
+  puts boardLineBottom;
+
+end
