1e57c1693db4ceca7c59001a0aa8aed75410d418
Newton Migosi <newtonmigosi@gmail.com>
2018-04-30 18:16:18 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-" * 11
+  puts" #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-" * 11
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
