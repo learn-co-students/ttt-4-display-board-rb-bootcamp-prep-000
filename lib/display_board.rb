55bc34b33f7032e658b2b53d39bc50525610ea57
Nick Bradley <nick.bradley.193@pm.me>
2018-05-01 05:06:21 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board_state)
+  puts " #{board_state[0]} | #{board_state[1]} | #{board_state[2]} "
+  puts "-----------"
+  puts " #{board_state[3]} | #{board_state[4]} | #{board_state[5]} "
+  puts "-----------"
+  puts " #{board_state[6]} | #{board_state[7]} | #{board_state[8]} "
+end
