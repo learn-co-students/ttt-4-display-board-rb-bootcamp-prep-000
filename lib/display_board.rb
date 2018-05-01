3cb31e14f6f5b57c5b705da99cc728015c53da18
Jared Chamberlain <jchamberlain909@gmail.com>
2018-05-01 18:45:59 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board (boardArray)
+  puts " #{boardArray[0]} | #{boardArray[1]} | #{boardArray[2]} "
+  puts "-----------"
+  puts " #{boardArray[3]} | #{boardArray[4]} | #{boardArray[5]} "
+  puts "-----------"
+  puts " #{boardArray[6]} | #{boardArray[7]} | #{boardArray[8]} "
+
+
+end
