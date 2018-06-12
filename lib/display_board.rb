f7a9e56f975142528fe93796ab535428d2959002
Trevor Jameson <tjameson15@gmail.com>
2018-06-12 21:14:45 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(moves)
+  board = " #{moves[0]} | #{moves[1]} | #{moves[2]} \n-----------\n #{moves[3]} | #{moves[4]} | #{moves[5]} \n-----------\n #{moves[6]} | #{moves[7]} | #{moves[8]} "
+  puts board
+end
