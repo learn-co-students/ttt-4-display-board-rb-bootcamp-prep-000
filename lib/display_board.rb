ae5da701c9349a50bbc05cb28d54b56162ff6758
jomartinezmarquez@gmail.com <jomartinezmarquez@gmail.com>
2018-06-16 22:55:06 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  top_row = " #{board[0]} | #{board[1]} | #{board[2]} "
+  mid_row = " #{board[3]} | #{board[4]} | #{board[5]} "
+  bot_row = " #{board[6]} | #{board[7]} | #{board[8]} "
+  dash = "-----------"
+  
+  puts "#{top_row}\n#{dash}\n#{mid_row}\n#{dash}\n#{bot_row}"
+end
