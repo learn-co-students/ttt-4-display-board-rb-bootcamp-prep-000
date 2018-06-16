a5c60a060c413bdb259dc17984191b3be0e4f6e4
Robin Börjesson <robin.b@outlook.com>
2018-06-16 12:31:06 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,14 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board = Array.new(9, " "))
+  row1 = " #{board[0]} | #{board[1]} | #{board[2]} "
+  row2 = " #{board[3]} | #{board[4]} | #{board[5]} "
+  row3 = " #{board[6]} | #{board[7]} | #{board[8]} "
+  sep = "-----------"
+  puts row1
+  puts sep
+  puts row2
+  puts sep
+  puts row3
+  
+end
