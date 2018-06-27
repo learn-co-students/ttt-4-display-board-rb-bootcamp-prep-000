39da8af8b85948fabde6d6abf669f9f148b2e0f7
gmat191@gmail.com <gmat191@gmail.com>
2018-06-27 02:35:23 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = [" "," "," "," "," "," "," "," "," "," "]
-display_board(board)
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts "   | #{board[4]} |   "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 
-def display_board
- 
