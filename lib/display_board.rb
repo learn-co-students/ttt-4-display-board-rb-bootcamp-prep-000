a10c8f8d853640622b815546b51f85113070cf51
Christopher Rouxel <chris.rouxel88@gmail.com>
2018-07-02 17:17:12 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,14 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(state)
+line="-----------"
+row_1=" #{state[0]} | #{state[1]} | #{state[2]} "
+row_2=" #{state[3]} | #{state[4]} | #{state[5]} "
+row_3=" #{state[6]} | #{state[7]} | #{state[8]} "
+puts row_1
+puts line
+puts row_2
+puts line
+puts row_3
+puts line
+end
