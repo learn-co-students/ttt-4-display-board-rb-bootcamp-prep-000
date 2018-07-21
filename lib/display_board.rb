22f2e95045a46f50b3f5e3976788729d45004c3b
Nalongsak Luangkhot <loongluangkhot@gmail.com>
2018-07-21 16:40:58 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(board)
+  demarcation_line = "-" * 11
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "#{demarcation_line}"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "#{demarcation_line}"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
