704cad3a7afad375e3602cec0dfd9dd7b86934c5
Logan Ripplinger <logan.ripplinger@gmail.com>
2018-07-26 19:46:56 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,16 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(board)
+  row_sep = "-"*11  
+  print " #{board[0]} | #{board[1]} | #{board[2]} "
+  print "\n"
+  print row_sep
+  print "\n"
+  print " #{board[3]} | #{board[4]} | #{board[5]} "
+  print "\n"
+  print row_sep
+  print "\n"
+  print " #{board[6]} | #{board[7]} | #{board[8]} "
+  print "\n"
+end
