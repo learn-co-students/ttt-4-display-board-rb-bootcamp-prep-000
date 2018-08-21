94ab590ac0854216795830a92eb493a5564cd4a9
Casey Zimmer <czimmer@ramapo.edu>
2018-08-21 00:49:02 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,16 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(board_string)
+  puts " #{board_string[0]} | #{board_string[1]} | #{board_string[2]} "
+  print_line_row
+  puts " #{board_string[3]} | #{board_string[4]} | #{board_string[5]} "
+  print_line_row
+  puts " #{board_string[6]} | #{board_string[7]} | #{board_string[8]} "
+end
+
+def print_line_row()
+  
+  puts "-----------"
+  
+end
