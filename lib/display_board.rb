f445cf5c7f8f27732a41f050d83cd65d43f10331
Travis Tokuyama <travistokuyama@gmail.com>
2018-08-24 01:07:13 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,16 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board (board)
+  horizontal_line = "-----------"
+  pipe_line = "|"
+  first_row = " " + "#{board[0]}" + " " + pipe_line + " " + "#{board[1]}" + " " + pipe_line + " " + "#{board[2]}" + " "
+  second_row = " " + "#{board[3]}" + " " + pipe_line + " " + "#{board[4]}" + " " + pipe_line + " " + "#{board[5]}" + " "
+  third_row = " " + "#{board[6]}" + " " + pipe_line + " " + "#{board[7]}" + " " + pipe_line + " " + "#{board[8]}" + " "
+  puts first_row
+  puts horizontal_line
+  puts second_row
+  puts horizontal_line
+  puts third_row
+end
+
