367418cd16a84e0aead4de02f636653a7b4f4020
Harry Bloch <harryfbloch@gmail.com>
2018-09-01 19:55:02 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def row(a = " ", b = " ", c = " ") 
+  " #{a} " + "|" +" #{b} " +  "|" + " #{c} \n"
+end
+
+def line 
+  "-----------\n"
+end
+
+def display_board(gameArray)
+ puts row(gameArray[0],gameArray[1],gameArray[2]) + line + row(gameArray[3],gameArray[4],gameArray[5]) + line + row(gameArray[6],gameArray[7],gameArray[8])
+end
