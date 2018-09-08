55e32bb37db5f2918ab37ea83ce29c414b8f5e25
Sean Lucius <seanlucius22@icloud.com>
2018-09-08 00:52:45 +0000

    Done.

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -165,11 +165,11 @@ describe "#display_board in 'lib/display_board.rb" do
 
       # *** Edit the lines below ***
       # *** Uncomment the lines below ***
-      # expect(rows[0]).to eq("   |   |   ")
-      # expect(rows[1]).to eq("-----------")
-      # expect(rows[2]).to eq("   |   |   ")
-      # expect(rows[3]).to eq("-----------")
-      # expect(rows[4]).to eq("   |   |   ")
+       expect(rows[0]).to eq("   |   |   ")
+       expect(rows[1]).to eq("-----------")
+       expect(rows[2]).to eq("   |   |   ")
+       expect(rows[3]).to eq("-----------")
+       expect(rows[4]).to eq("   |   |   ")
 
       # *** Comment the line below by adding a # at the line start ***
       expect(true).to be(true)
