3a2dfa43bc5a2046264ffc4a8480c21152dc4d72
viqi zevic <vicky.tanzil@gmail.com>
2018-09-09 09:54:01 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,10 @@
 
 def display_board(board)
-  col_sep = "|"
-  row_sep = "-----------"
-  row1 = " #{board[0]} #{col_sep} #{board[1]} #{col_sep} #{board[2]} "
-  row2 = " #{board[3]} #{col_sep} #{board[4]} #{col_sep} #{board[5]} "
-  row3 = " #{board[6]} #{col_sep} #{board[7]} #{col_sep} #{board[8]} "
-  puts row1
-  puts row_sep
-  puts row2
-  puts row_sep
-  puts row3
+  3.times do |i|
+    j = i * 3
+    puts " #{board[j]} | #{board[j+1]} | #{board[j+2]} "
+    if i < 3
+      puts "-----------"
+    end
+  end
 end
