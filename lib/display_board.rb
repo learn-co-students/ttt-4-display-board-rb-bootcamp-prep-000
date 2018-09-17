aac26b425d4c8195f31f9f9733be2ac2756166b6
Suheyl Turac <skbsturac@live.com>
2018-09-17 03:08:48 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(board)
+  separator = "|"
+  lines = "-----------"
+  
+  puts "#{board[0]} #{separator} #{X}  #{separator} #{" "}"
+  puts "#{lines}"
+  puts "#{" "} #{separator} #{X}  #{separator} #{" "}"
+  puts "#{lines}"
+  puts "#{X} #{separator} #{O}  #{separator} #{" "}"
+end
