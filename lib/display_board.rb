4ad2e076f15c22523fc13a2b94c315e7294a3c8e
Sarah Yun <sarahyun7@gmail.com>
2018-11-30 00:21:29 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,14 +2,13 @@
 # out the current state.
 
 
-def display_board
-  puts " '   '|'   '|'   ' " 
+board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} " 
   puts "-----------"
-  puts " '   '|'   '|'   ' " 
+  puts " #{board[3]} | #{board[4]} | #{board[5]} " 
   puts "-----------"
-  puts " '   '|'   '|'   ' " 
-  
+  puts " #{board[6]} | #{board[7]} | #{board[8]} " 
 end
 
-
-board = []
