5401be6ca181357fa0bcc71734c482cf343f5002
camille feghali <camfeg@gmail.com>
2019-01-23 21:53:33 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,14 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board (board)
+  position = 0
+  board.each_with_index do |value, index|
+    if index == 2 || index == 5 || index == 8
+      print " #{value} \n"
+      puts "-----------"
+    else
+      print " #{value} |"
+    end
+  end
+  puts "\n"
+end
