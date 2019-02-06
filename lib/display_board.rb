1385a4cc38e52dd496b69c157ee140ae4bb29d08
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-06 04:06:14 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(state)
+  puts " #{state[0]} | #{state[1]} | #{state[2]} "
+  puts "-----------"
+  puts " #{state[3]} | #{state[4]} | #{state[5]} "
+  puts "-----------"
+  puts " #{state[6]} | #{state[7]} | #{state[8]} "
+end
