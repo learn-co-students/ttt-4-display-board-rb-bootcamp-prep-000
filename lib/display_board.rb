b760f399ed60f9eb937882c43d4de1f57509fbf8
Jonathan Koss <jonathan_koss@yahoo.com>
2019-04-23 15:36:12 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,5 +2,12 @@
 # out the current state.
 
 def display_board(board)
-  
+  rows = [
+    " #{board[0]} | #{board[1]} | #{board[2]} ",
+    "-----------",
+    " #{board[3]} | #{board[4]} | #{board[5]} ",
+    "-----------",
+    " #{board[6]} | #{board[7]} | #{board[8]} "
+    ]
+    puts rows
 end
