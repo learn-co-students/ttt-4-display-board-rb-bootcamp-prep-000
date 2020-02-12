c950648707ba7263a2e41f32ac7f6edd0c69ff19
sensible-draft-9261 <jasmine.williamson@me.com>
2020-02-12 18:28:07 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,20 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+   board=[" "," "," "," "," "," "," "," "," "]
+   board=[" "," "," "," "," X "," "," "," "," "]
+   board=["O"," "," "," "," "," "," "," "," "]
+   board=["O"," "," "," ","X"," "," "," "," "]
+   board=[" X "," X "," X "," "," "," "," "," "," "]
+   board=[" "," "," "," "," "," "," O "," O "," O "]
+   board=[" X "," "," "," "," X "," "," "," "," X "]
+   board=[" "," "," O "," "," O "," "," O "," "," "]
+   board=[" "," O "," "," "," X "," "," "," X "," "]
+   board=[" X "," X "," X "," X "," X "," X "," X "," X "," X "]
+   board=[" O "," O "," O "," O "," O "," O "," O "," O "," O "]
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+ end
