2dbf4c06b149edde89d5876eaf6f0d402d540e84
masterful-array-3850 <crgammage@gmail.com>
2020-04-13 19:46:55 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,37 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def display_board(board)
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# puts "Turn 1"
+# rows = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+# display_board(rows)
+#
+# puts "Turn 2"
+# board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+# display_board(board)
+#
+# board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+# display_board(board)
+#
+# board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+# display_board(board)
+#
+# board = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
+# display_board(board)
+#
+# board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+# display_board(board)
+#
+# board = [" ", " ", "O", " ", "O", " ", "O", " ", " "]
+# display_board(board)
+#
+# board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
+# display_board(board)
+#
+# board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
+# display_board(board)
