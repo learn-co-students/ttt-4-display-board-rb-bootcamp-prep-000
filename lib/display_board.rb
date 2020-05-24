b10bb158de9f0cc17f9671d0b11f536bafc6cdfc
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-24 19:22:32 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,28 +2,11 @@
 
 def display_board(board)
 
-  puts "   |   |   "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 
 
-
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-
-
-
-
-
-
-
-
-
-
- 
 end
