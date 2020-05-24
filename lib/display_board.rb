72ea9aed304bc4617d8b30c2b7d71089684f92a8
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-24 18:06:02 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,21 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+# Define display_board that accepts a board and prints out the current state.
+
+def display_board
+
+colors = ["Red","Yellow", "Green"]
+puts "The first color is #{colors[0]}"
+
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+
+
+
+ 
+end
